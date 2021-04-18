21c806b6800c2f6ff5ec0615a86c0fefd47f2555
elTrillos <strillos@miuandes.cl>
2021-04-18 15:32:20 -0400

    agregado el sistema de ID de las cartas

--- card.py
+++ card.py
@@ -1,7 +1,17 @@
 
 
 class Card():
-    def __init__(self):
-        pass
+    def __init__(self,cardID):
+        self.__cardID = cardID
 
-    #def gameplay(self):
+    
+    @property
+    def getid(self):
+        return self.__cardID
+
+    @cardID.setter
+    def cardID(self,newID):
+        self.__cardID = newID
+
+    def cardprint(self):
+        print(self.__cardID)
