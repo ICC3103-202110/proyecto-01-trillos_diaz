f9c6d44d0a954e3cd9eadf7b17b4b2b468dbf4a2
elTrillos <strillos@miuandes.cl>
2021-04-18 20:03:14 -0400

    Mejorando deck y card, creo que ando por el buen camino

--- card.py
+++ card.py
@@ -3,18 +3,35 @@
 class Card():
     def __init__(self,cardID):
         self.__cardID = cardID
+        self.__cardStatus = "hidden"
 
-
-    
     @property
-    def getid(self):
+    def cardID(self):
         return self.__cardID
 
-    """
-    @cardID.setter
-    def cardID(self,newID):
+    
+    def cardIDSet(self,newID):
         self.__cardID = newID
-    """
+
+    @property
+    def cardStatus(self):
+        return self.__cardStatus
+
+    def cardStatusSet(self):
+        print("yeet")
+        if self.__cardStatus == "hidden":
+
+            self.__cardStatus = "shown"
+
     def cardprint(self):
         print(self.__cardID)
+    
+    def cardStatusShow(self):
+        print(self.__cardStatus)
+
+card1 = Card(1)
+card1.cardStatusShow()
+card1.cardStatusSet()
+card1.cardStatusShow()
+
     
